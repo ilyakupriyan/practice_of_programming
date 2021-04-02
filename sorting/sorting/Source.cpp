a00f061e45cd7e073517aa1fb9f1f45f14851097
Ilya <kupriyanov_2002@mail.ru>
2021-04-02 22:17:27 +0300

    Changing cocktale sorting to quick sorting

--- sorting/sorting/Source.cpp
+++ sorting/sorting/Source.cpp
@@ -10,36 +10,45 @@ void swap(uint16_t* var1, uint16_t* var2) {
 	*var2 = temp;
 }
 
-void cocktailSort(uint16_t* arr, int size) {
-	int left_lim = 0, right_lim = size - 1, count = 0;
-	while (left_lim <= right_lim) {
-		for (int i = left_lim; i < right_lim; i++)
-			if (arr[i] > arr[i + 1])
-				swap(&arr[i], &arr[i + 1]);
-		right_lim--;
-		for (int i = right_lim; i > left_lim; i--)
-			if (arr[i] < arr[i - 1])
-				swap(&arr[i], &arr[i - 1]);
-		left_lim++;
-		count++;
+void qSort(uint16_t* arr, int left_lim, int right_lim, int* count) {
+	if (left_lim < right_lim) {
+		int l_lim = left_lim, r_lim = right_lim, //переменные для хранения индексов массива, которые нужно перекинуть на другую сторону от опорного элемента, и границ
+			mid = (arr[left_lim] + arr[right_lim]) / 2; //вычисление опорного элемента
+		while (l_lim < r_lim) {
+			//находим элемент слева от опорного, который нужно перебросить на правую сторону
+			while (mid > arr[l_lim]) l_lim++;
+			//находим элемент справа от опорного, который нужно перебросить на левую сторону
+			while (mid < arr[r_lim]) r_lim--;
+			//меняем местами выбранные элементы,если требуется
+			if (l_lim <= r_lim) {
+				swap(&arr[l_lim], &arr[r_lim]);
+				r_lim--;
+				l_lim++;
+				*count += 1;
+			}
+		}
+		//запускаем рекурсию для двух частей массива
+		if (left_lim < r_lim) qSort(arr, left_lim, r_lim, count);
+		if (right_lim > l_lim) qSort(arr, l_lim, right_lim, count);
 	}
-	cout << "Number of iterations: " << count << endl;
 }
 
 int main() {
 	srand(time(NULL));
 	int16_t size, max;
+	int count = 0;
 	cout << "Enter the size of array and maximum possible number" << endl;
 	cin >> size >> max;
 	uint16_t* array = (uint16_t*) calloc (size, sizeof(int));
 	for (int i = 0; i < size; i++) {
-		array[i] = rand() % max;
+		array[i] = rand() % max + 1;
 		cout << array[i] << " ";
 	}
 	cout << endl;
-	cocktailSort(array, size);
+	qSort(array, 0, size-1, &count);
 	for (int i = 0; i < size; i++) 
 		cout << array[i] << " ";
+	cout << endl << "Number of iterations of sorting: " << count;
 	free(array);
 	_getch();
 	return 0;
