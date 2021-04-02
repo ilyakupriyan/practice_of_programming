44c0f1c3bd67d69f2b1256d4d1bc76250f4d4e0f
Ilya <kupriyanov_2002@mail.ru>
2021-04-02 11:09:59 +0300

    Changing bubble sorting to cocktail sorting

--- sorting/sorting/Source.cpp
+++ sorting/sorting/Source.cpp
@@ -4,40 +4,43 @@
 
 using namespace std;
 
-void swap (int* var1, int* var2) {
+void swap(uint16_t* var1, uint16_t* var2) {
 	int temp = *var1;
 	*var1 = *var2;
 	*var2 = temp;
 }
 
-void bubbleSort(int* array) {
-	int flag = 0;
-	for (int i = 0; i < 9; i++) {
-		flag = 0;
-		for (int a = 0; a < 9 - i; a++) {
-			if (array[a] > array[a + 1]) {
-				swap(&array[a], &array[a + 1]);
-				flag = 1;
-			}
-		}
-		if (!flag)
-			break;
+void cocktailSort(uint16_t* arr, int size) {
+	int left_lim = 0, right_lim = size - 1, count = 0;
+	while (left_lim <= right_lim) {
+		for (int i = left_lim; i < right_lim; i++)
+			if (arr[i] > arr[i + 1])
+				swap(&arr[i], &arr[i + 1]);
+		right_lim--;
+		for (int i = right_lim; i > left_lim; i--)
+			if (arr[i] < arr[i - 1])
+				swap(&arr[i], &arr[i - 1]);
+		left_lim++;
+		count++;
 	}
-		
+	cout << "Number of iterations: " << count << endl;
 }
 
 int main() {
 	srand(time(NULL));
-	int mass[10];
-	for (int i = 0; i <= 9; i++) {
-		mass[i] = rand() % 50;
-		cout << mass[i] << " ";
+	int16_t size, max;
+	cout << "Enter the size of array and maximum possible number" << endl;
+	cin >> size >> max;
+	uint16_t* array = (uint16_t*) calloc (size, sizeof(int));
+	for (int i = 0; i < size; i++) {
+		array[i] = rand() % max;
+		cout << array[i] << " ";
 	}
 	cout << endl;
-	bubbleSort(mass);
-	for (int i = 0; i <= 9; i++) {
-		cout << mass[i] << " ";
-	}
+	cocktailSort(array, size);
+	for (int i = 0; i < size; i++) 
+		cout << array[i] << " ";
+	free(array);
 	_getch();
 	return 0;
 }
